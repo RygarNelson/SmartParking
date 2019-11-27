8f8db4d163798d3af8aab8ad8fedea3a30e5f903
Luca <luca.marasca@studenti.unicam.it>
2019-11-27 17:21:55 +0100

    Creazione app Android

---
+++ client/Client_Android/app/src/main/java/com/example/pick_a_park/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pick_a_park;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
